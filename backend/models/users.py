d36735208b3c47c7aaa165d446e7c5cbc3c998b4
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 00:58:14 +0530

    commited by n8n

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,14 +1,12 @@
 from datetime import datetime
 from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
 
 class User(BaseModel):
     id: int = Field(..., primary_key=True, autoincrement=True)
     email: EmailStr
     password: str
-    mfa_enabled: bool = Field(..., default=False)
-    mfa_secret: Optional[str] = Field(None)
-    is_active: bool = Field(..., default=True)
+    first_name: str
+    last_name: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
     
