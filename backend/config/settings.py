de008b8b190bd382a21961fb7e44f40f01829ef8
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 09:02:11 +0530

    commited by n8n

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,5 +3,6 @@ import os
 class Settings:
     DATABASE_PATH: str = os.getenv("DATABASE_PATH", "database/app.db")
     DEBUG: bool = bool(os.getenv("DEBUG", True))
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
 
 settings = Settings()
