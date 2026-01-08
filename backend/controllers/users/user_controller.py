a77ab1931be941b682ad5f2a26f0385be6a43b21
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 10:27:21 +0530

    commited by n8n

--- backend/controllers/users/user_controller.py
+++ backend/controllers/users/user_controller.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, request, jsonify
 from backend.services.auth.auth_service import AuthService
+from backend.services.profile.profile_service import ProfileService
 from backend.repositories.users.user_repository import UserRepository
 from backend.repositories.sessions.session_repository import SessionRepository
 from backend.repositories.password_reset.password_reset_repository import PasswordResetRepository
+from backend.repositories.profile.profile_repository import ProfileRepository
 
 user_blueprint = Blueprint('user', __name__)
 user_repository = UserRepository()
 session_repository = SessionRepository()
 password_reset_repository = PasswordResetRepository()
+profile_repository = ProfileRepository()
 auth_service = AuthService(user_repository, session_repository, password_reset_repository)
+profile_service = ProfileService(profile_repository)
 
 @user_blueprint.route('/register', methods=['POST'])
 def register():
@@ -45,3 +49,25 @@ def reset_password():
     new_password = data.get('new_password')
     user = auth_service.reset_password(token, new_password)
     return jsonify(user.dict()), 200
+
+@user_blueprint.route('/profile', methods=['POST'])
+def create_profile():
+    data = request.json
+    profile = profile_service.create_profile(data)
+    return jsonify(profile.dict()), 201
+
+@user_blueprint.route('/profile/<int:profile_id>', methods=['PUT'])
+def update_profile(profile_id):
+    data = request.json
+    profile = profile_service.update_profile(profile_id, data)
+    return jsonify(profile.dict()), 200
+
+@user_blueprint.route('/profile/<int:profile_id>', methods=['GET'])
+def get_profile(profile_id):
+    profile = profile_service.get_profile(profile_id)
+    return jsonify(profile.dict()), 200
+
+@user_blueprint.route('/profile/<int:profile_id>', methods=['DELETE'])
+def delete_profile(profile_id):
+    profile_service.delete_profile(profile_id)
+    return jsonify({"message": "Profile deleted"}), 200
