7d59e443df0970dae1d708c501f31bdd29d1226f
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 00:58:11 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask
-from flask_socketio import SocketIO, emit
-from backend.controllers.users.notification_controller import notification_controller
-
-socketio = SocketIO()
+from backend.controllers.users.account_controller import account_controller
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.register_blueprint(notification_controller, url_prefix='/user')
-    socketio.init_app(app)
+    app.register_blueprint(account_controller, url_prefix='/user')
     return app
-
-@socketio.on('connect')
-def handle_connect():
-    emit('message', {'message': 'Connected successfully'})
 
 if __name__ == '__main__':
     app = create_app()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
