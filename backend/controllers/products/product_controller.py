3b0f2fbc34e87b5884a94fda97795a2f92683fbb
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 10:29:09 +0530

    commited by n8n

--- backend/controllers/products/product_controller.py
+++ backend/controllers/products/product_controller.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, jsonify
 from backend.services.product_catalog.product_service import ProductService
 from backend.repositories.products.product_repository import ProductRepository
+from backend.repositories.categories.category_repository import CategoryRepository
 
 product_blueprint = Blueprint('product', __name__)
 product_repository = ProductRepository()
-product_service = ProductService(product_repository)
+category_repository = CategoryRepository()
+product_service = ProductService(product_repository, category_repository)
 
 @product_blueprint.route('/products', methods=['POST'])
 def add_product():
@@ -44,3 +46,30 @@ def search_products():
     page_size = int(request.args.get('page_size', 10))
     products = product_service.search_products(query, page, page_size)
     return jsonify([product.dict() for product in products]), 200
+
+@product_blueprint.route('/categories', methods=['POST'])
+def add_category():
+    data = request.json
+    category = product_service.add_category(data)
+    return jsonify(category.dict()), 201
+
+@product_blueprint.route('/categories/<int:category_id>', methods=['PUT'])
+def update_category(category_id):
+    data = request.json
+    category = product_service.update_category(category_id, data)
+    return jsonify(category.dict()), 200
+
+@product_blueprint.route('/categories/<int:category_id>', methods=['GET'])
+def get_category(category_id):
+    category = product_service.get_category(category_id)
+    return jsonify(category.dict()), 200
+
+@product_blueprint.route('/categories', methods=['GET'])
+def list_categories():
+    categories = product_service.list_categories()
+    return jsonify([category.dict() for category in categories]), 200
+
+@product_blueprint.route('/categories/<int:category_id>', methods=['DELETE'])
+def delete_category(category_id):
+    product_service.delete_category(category_id)
+    return jsonify({"message": "Category deleted"}), 200
