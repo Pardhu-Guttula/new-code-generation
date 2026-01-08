153c5a59227ce816d81350c6c11c45e1d0090ad3
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 09:01:17 +0530

    commited by n8n

--- backend/models/users/user.py
+++ backend/models/users/user.py
@@ -6,6 +6,9 @@ class User:
     id: int
     email: str
     password: str
+    first_name: str
+    last_name: str
+    phone_number: str
     login_attempts: int
     is_locked: bool
     last_login_at: datetime
