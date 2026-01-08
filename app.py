48bf740973439f5dd7e15cee10306d20a2b0158b
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 00:58:04 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from backend.controllers.sync.sync_controller import sync_controller
+from backend.controllers.users.role_controller import role_controller
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.register_blueprint(sync_controller, url_prefix='/api')
+    app.register_blueprint(role_controller, url_prefix='/user')
     return app
 
 if __name__ == '__main__':
