de137f936d2966cb6f9983d53b8deda0a47ce414
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-08 09:00:24 +0530

    commited by n8n

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,5 +3,7 @@ import os
 class Settings:
     DATABASE_PATH: str = os.getenv("DATABASE_PATH", "database/app.db")
     DEBUG: bool = bool(os.getenv("DEBUG", True))
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey")
+    PERMANENT_SESSION_LIFETIME: int = int(os.getenv("PERMANENT_SESSION_LIFETIME", 30 * 60))  # 30 minutes
 
 settings = Settings()
