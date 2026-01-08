4bccc2789b42584aa2be6907db376a2b11d98b68
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 00:58:16 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from backend.controllers.users.account_controller import account_controller
+from backend.controllers.users.dashboard_controller import dashboard_controller
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.register_blueprint(account_controller, url_prefix='/user')
+    app.register_blueprint(dashboard_controller, url_prefix='/user')
     return app
 
 if __name__ == '__main__':
