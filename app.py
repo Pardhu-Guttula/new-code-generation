773e12a8d644aff91d6d0fe58b68ccc476774a6b
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 00:58:03 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from backend.controllers.data.data_controller import data_controller
+from backend.controllers.sync.sync_controller import sync_controller
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.register_blueprint(data_controller, url_prefix='/data')
+    app.register_blueprint(sync_controller, url_prefix='/api')
     return app
 
 if __name__ == '__main__':
