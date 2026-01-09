1b56d9e5891d9eba49f2597c648edbdf43a541f0
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 13:18:17 +0530

    committed by n8n

---
+++ digital-banking-portal/backend/repositories/user/repositories.py
@@ -0,0 +1,30 @@
+from digital-banking-portal.backend.models.user.models import User
+from digital-banking-portal.backend import db
+
+class UserRepository:
+
+    @staticmethod
+    def add_user(user: User) -> None:
+        db.session.add(user)
+        db.session.commit()
+
+    @staticmethod
+    def get_user_by_id(user_id: int) -> User:
+        return User.query.get(user_id)
+
+    @staticmethod
+    def get_user_by_username(username: str) -> User:
+        return User.query.filter_by(username=username).first()
+
+    @staticmethod
+    def get_user_by_email(email: str) -> User:
+        return User.query.filter_by(email=email).first()
+
+    @staticmethod
+    def update_user() -> None:
+        db.session.commit()
+
+    @staticmethod
+    def delete_user(user: User) -> None:
+        db.session.delete(user)
+        db.session.commit()
