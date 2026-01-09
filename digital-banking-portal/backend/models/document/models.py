b198dc73e5d4a1731e509f42416c0837ef4e3a14
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 13:18:16 +0530

    committed by n8n

---
+++ digital-banking-portal/backend/models/document/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from digital-banking-portal.backend.models import db
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    file_path = db.Column(db.String(100), nullable=False)
+    uploaded_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    uploaded_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Document('{self.title}', '{self.file_path}')"
