a2e760f1ff8048e73993330b0e27bef360093828
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 13:18:46 +0530

    commited by n8n

--- digital-banking-portal/backend/models/user/models.py
+++ digital-banking-portal/backend/models/user/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from digital-banking-portal.backend.models import db
+from digital_banking_portal.backend.models import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
