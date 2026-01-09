f44f0404e178d2d9ebca61eb79441b0a8e1993ab
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 13:18:49 +0530

    commited by n8n

--- digital-banking-portal/backend/models/notification/models.py
+++ digital-banking-portal/backend/models/notification/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from digital-banking-portal.backend.models import db
+from digital_banking_portal.backend.models import db
 
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
