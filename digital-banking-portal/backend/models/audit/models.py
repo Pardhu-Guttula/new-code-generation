d2befe6072ae4e86432e53aa435c9bd3e7c92d08
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-01-09 13:18:17 +0530

    committed by n8n

---
+++ digital-banking-portal/backend/models/audit/models.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from digital-banking-portal.backend.models import db
+
+class AuditLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    action = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"AuditLog('{self.action}', 'User ID: {self.user_id}', 'Timestamp: {self.timestamp}')"
